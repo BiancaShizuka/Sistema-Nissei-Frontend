8ad76c83fd4a730eb04eb619e712ec9383a4d2e0
Bianca <umemurabianca@gmail.com>
2021-06-16 10:49:48 -0300

    estilo cadastro despesa

--- src/Paginas/Cadastros/cadastroDespesa.js
+++ src/Paginas/Cadastros/cadastroDespesa.js
@@ -1,45 +1,56 @@
 import api from '../../servicos/api';
 import React,{useState,useEffect} from 'react';
 import history from '../../history'
-import './cadastroContato.css'
+import './cadastroDespesa.css'
 import '../../app.css'
 import Header from '../../Components/Header'
 
-function FormularioDespesa(){
+function CadastroDespesa(){
     const [nivel,setNivel] =useState('');
 
     function gravarDespesa(){
 
     }
     function voltarHome(){
-
+        history.goBack();
     }
     return(
-        <div></div>
-        /*
         <div className="background">
             <Header/>
             <div className="div-cadDespesas">
-                <form class='form-despesas' onSubmit={gravarDespesa}>
-                    <label>Tipo de despesas:</label>
-                    <input type="text" id="tpDespesa" name="tpDespesas"/>
-                    <label>Data de Lançamento:</label>
-                    <input type="date" id="dtLancamento" name="dtLancamento"/>
-                    <label>Data de Vencimento:</label>
-                    <input type="date" id="dtVencimento" name="dtVencimento/"/>
-                    <label>Modo de Pagamento:</label>
-                    <input type="radio" id="avista" name="avista" value="A" checked={nivel==='A'}/>
-                    <label for="avista">À vista</label>
-                    <input type="radio" id="parcelado" name="parcelado" value="P" />
-                    <label for="parcelado">Parcelado</label>
-                    <label>Número de Parcelas:</label>
-                    <input type="number" id="NParcelas" name="NParcelas"/>
-                    <button type="submit" class="buttonSave">Salvar</button>
-                    <button type="button" onClick={voltarHome} class="buttonBack">Voltar</button>
+                <h1>Cadastro Despesa</h1>
+                <form className='form-despesas' onSubmit={gravarDespesa}>
+                    <div className="input-block blockDes-tp">
+                        <label>Tipo de despesas:</label>
+                        <input type="text" id="tpDespesa" name="tpDespesas"/>
+                    </div>
+                    <div className="input-block blockDes-dtLanc">
+                        <label>Data de Lançamento:</label>
+                        <input type="date" id="dtLancamento" name="dtLancamento"/>
+                    </div>
+                    <div className="input-block blockDes-dtVenc">
+                        <label>Data de Vencimento:</label>
+                        <input type="date" id="dtVencimento" name="dtVencimento"/>
+                    </div>
+                    <div className="input-block blockDes-tpPag">
+                        <label>Modo de Pagamento:</label>
+                        <label>À vista
+                            <input type="radio" id="tpPag" name="tpPag" value="A" checked={nivel==='A'}/>
+                        </label>
+                        <label>Parcelado
+                            <input type="radio" id="tpPag" name="tpPag" value="P" />
+                        </label>
+                    </div>
+                    <div className="input-block blockDes-nParc">
+                        <label>Número de Parcelas:</label>
+                        <input type="number" id="NParcelas" name="NParcelas"/>
+                    </div>
+                    <button type="submit" className="buttonSave">Salvar</button>
                 </form>
-            </div> 
                 
-        </div>  */ 
+            </div> 
+            <button type="button" onClick={voltarHome} className="buttonBack">Voltar</button>
+        </div>  
     );
 }
-export default FormularioDespesa;
+export default CadastroDespesa;
